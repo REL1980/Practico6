b56f99ce41e85c77dad6d88cc0c04f661472269b
REL-Computación <REL-Computacin@Rafael>
2020-09-29 23:33:00 -0300

    segundo commit

--- src/practico6/Matriz.java
+++ src/practico6/Matriz.java
@@ -9,25 +9,21 @@ public class Matriz
         arreglo[1] = new int[3];
         arreglo[2] = new int[5];
         arreglo[3] = new int[2];
-        
-        arreglo[0][0] = 3;
-        arreglo[0][1] = 6;
-        arreglo[0][2] = 9;
-        arreglo[0][3] = 12;
-        
-        arreglo[1][0] = 15;
-        arreglo[1][1] = 18;
-        arreglo[1][2] = 21;
-        
-        arreglo[2][0] = 24;
-        arreglo[2][1] = 27;
-        arreglo[2][2] = 30;
-        arreglo[2][3] = 33;
-        arreglo[2][4] = 36;
-        
-        arreglo[3][0] = 39;
-        arreglo[3][1] = 42;
-        
+        System.out.println("----------------------");
+        System.out.println("Arreglo bidimensional");
+        int contador = 3;
+        for (int fila=0;fila<arreglo.length;fila++) 
+        {
+            for (int columna = 0; columna < arreglo[fila].length; columna++) 
+            {
+                System.out.print((arreglo[fila][columna] = contador) + " ");
+                contador = contador + 3;
+                
+            }
+            System.out.println("");
+        }
+        System.out.println("----------------------");
+        System.out.println("Solo los pares:");             
         for (int[] arr : arreglo) 
         {
             for (int col = 0; col < arr.length; col++) 
